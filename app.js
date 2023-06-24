1d2b12f372ea841387acad35cb61d46f276e1fdb
C1330091 Leandro Santos Lopes <c1330091@interno.com.br>
2023-06-24 09:00:10 -0300

    Various changes

--- app.js
+++ app.js
@@ -2,16 +2,32 @@
     'use strict'
 
     angular.module('MyApp', [])
-        .controller('MyAppController', ['$scope', '$filter', MyAppController]);
+        .controller('MyAppController', MyAppController)
+        .filter('loves', LovesFilter);
         
-        MyAppController.$inject = ['$scope', '$filter'];
+        MyAppController.$inject = ['$scope', '$filter', 'lovesFilter'];
 
-        function MyAppController($scope, $filter) {
+        function MyAppController($scope, $filter, lovesFilter) {
             $scope.name = 'Lopes';
             $scope.stateOfBeing = 'hungry';
+            $scope.cookieCost = .45;
 
             $scope.feed = function() {
                 $scope.stateOfBeing = 'nohungry';
             };
+
+            $scope.sayLovesMessage = function() {
+                var msg = 'Leandro likes Kerly!!!';
+                //msg = lovesFilter(msg);
+                return msg;
+            };
+        }
+
+        function LovesFilter() {
+            return function (input) {
+                input = input || "";
+                input = input.replace("likes", "loves");
+                return input;
+            }
         }
 })();
